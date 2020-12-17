3c95a69d1bab6bef502bfa22d90b51c290e2a3da
shashikant <shashikantilhe@gmail.com>
2020-12-17 14:27:13 +0530

    master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UserSignUpComponent } from '../app/user-sign-up/user-sign-up.component'
+import { FarmComponent } from './entities/farm/farm.component';
+import { FarmerComponent } from './entities/farmer/farmer.component';
+import { CropComponent } from './entities/crop/crop.component';
+import { MarketComponent } from './entities/market/market.component';
+import { LaboursComponent } from './entities/labours/labours.component';
+import { WarehouseComponent } from './entities/warehouse/warehouse.component';
 const routes:Routes = [
-  {component: UserSignUpComponent, path: 'register'}
+  { path: 'farmer', component: FarmerComponent},
+  { path: 'farm', component: FarmComponent},
+  { path: 'crop', component: CropComponent},
+  { path: 'market', component: MarketComponent},
+  { path: 'labours', component: LaboursComponent}, 
+  { path: 'warehouse', component: WarehouseComponent }
 ];
 
 @NgModule({
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router'
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
@@ -7,9 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
-  constructor() { }
+  navLinks: any[];
+  activeLinkIndex = -1;
+
+  constructor(private router: Router) {
+    this.navLinks = [
+      {
+          label: 'Farmer',
+          link: './farmer',
+          index: 0
+      }, {
+          label: 'Farm',
+          link: './farm',
+          index: 1
+      }, {
+          label: 'Crop',
+          link: './crop',
+          index: 2
+      }, {
+          label: 'Market',
+          link: './market',
+          index: 3
+      }, {
+          label: 'Labours',
+          link: './labours',
+          index: 4
+      },  {
+          label: 'Warehouse',
+          link: './warehouse',
+          index: 5
+/**      },  {
+          label: 'Pesticides',
+          link: './pesticides',
+          index: 6
+      },  {
+          label: 'Fertilizers',
+          link: './fertilizers',
+          index: 7
+      },  {
+          label: 'Seeds',
+          link: './seeds',
+          index: 8
+      },  {
+          label: 'Animal Husbandary',
+          link: './animalHusbandary',
+          index: 9       **/
+      },  
+    ];
+  }
 
   ngOnInit(): void {
+    this.router.events.subscribe((res) => {
+      this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === '.' + this.router.url));
+  });
   }
 
 }
